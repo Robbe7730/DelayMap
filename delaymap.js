5f4d056e2b93999921a5f777bb93835d1b8d7fcd
Robbe Van Herck <robbe@robbevanherck.be>
2021-02-09 17:04:09 +0100

    Remove old backend and port frontend to new backend

--- delaymap.js
+++ delaymap.js
@@ -3,6 +3,8 @@ let interval;
 let markers;
 let stats_control;
 
+const API_URL = "http://localhost:8000/trains";
+
 function onLoad() {
   map = L.map('leafletMap').setView([50.502, 4.335], 8);
 
@@ -31,27 +33,32 @@ function addLegend(map) {
 }
 
 function getTrains() {
-  return fetch("/trains.json").then((res) => res.json())
-                              .then(drawData);
+  return fetch(API_URL).then((res) => res.json())
+                       .then(drawData);
 }
 
 function drawData(data) {
-  drawTrains(data["trains"]);
-  drawStats(data["stats"]);
+  drawTrains(data);
+  drawStats(data);
 }
 
-function drawStats(stats_data) {
+function drawStats(trains) {
   stats_control.remove()
   stats_control = L.control({position: 'bottomleft'});
-  stats_control.onAdd = (map) => addStats(map,stats_data);
+  stats_control.onAdd = (map) => addStats(map,trains);
   stats_control.addTo(map);
 }
 
-function addStats(map, stats) {
+function addStats(map, trains) {
   let green = 0;
   let orange = 0;
   let red = 0;
-  stats["all_delays"].forEach((delay) => {
+  let total_delay = 0
+  let max_delay = 0;
+  trains.forEach((train) => {
+    const curr_station = train.stops[train.stop_index];
+    const delay = train.is_stopped ? curr_station.departure_delay : curr_station.arrival_delay;
+    total_delay += delay;
     if (delay == 0) {
       green++;
     } else if (delay <= 360) {
@@ -59,11 +66,15 @@ function addStats(map, stats) {
     } else { 
       red++;
     }
+    if (delay > max_delay) {
+      max_delay = delay;
+    }
   });
+  const avg_delay = total_delay / trains.length;
   const div = L.DomUtil.create('div', 'info legend');
   div.innerHTML = "<strong>Stats</strong><br>" +
-                  `Average delay: ${Math.round(stats["avg_delay"] / 0.60) / 100} minutes <br>` + 
-                  `Maximum delay: ${stats["max_delay"] / 60} minutes <br>` +
+                  `Average delay: ${Math.round(avg_delay / 0.60) / 100} minutes <br>` + 
+                  `Maximum delay: ${max_delay / 60} minutes <br>` +
                   `"Green" trains: ${green} <br>` +
                   `"Orange" trains: ${orange} <br>` +
                   `"Red" trains: ${red} <br>` +
@@ -79,15 +90,17 @@ function drawTrains(trains) {
 }
 
 function drawTrain(train) {
-  const color = train.delay == 0 ? "green" : (train.delay <= 360 ? "orange" : "red")
+  const curr_station = train.stops[train.stop_index];
+  const curr_delay = train.is_stopped ? curr_station.departure_delay : curr_station.arrival_delay;
+  const color = curr_delay == 0 ? "green" : (curr_delay <= 360 ? "orange" : "red")
   const trainMarker = L.divIcon({
     html: `<i class="fa fa-train" style="color: ${color}"></i>`,
     iconSize: [20, 20],
     className: 'myDivIcon',
     popupAnchor: [-15,-15]
   })
-  const marker = L.marker([train.lat, train.lon], {icon: trainMarker}).addTo(markers);
-  marker.bindPopup(`<strong>${train.name}</strong>: +${train.delay/60} min<br>Next stop: ${train.nextStopName}`);
+  const marker = L.marker([train.estimated_lat, train.estimated_lon], {icon: trainMarker}).addTo(markers);
+  marker.bindPopup(`<strong>${train.name}</strong>: +${curr_delay/60} min<br>Next stop: ${curr_station.name}`);
   marker.on('mouseover', (e) => {
     marker.openPopup();
   });
