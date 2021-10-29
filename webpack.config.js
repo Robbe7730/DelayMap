ca3b4254f6ba61cb7fac1b63896bf8c5dc833507
Robbe Van Herck <robbe@robbevanherck.be>
2021-10-29 20:50:48 +0200

    Fix docker build

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,6 @@ module.exports = {
           { from: "static", to: "static" },
           { from: "src/index.html", to: "" },
           { from: "src/style.css", to: "" },
-          { from: "config/config.json", to: "" },
         ],
       }),
     ],
